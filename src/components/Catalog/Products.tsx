ede74d1b154f6e1f4b65620d47c492a04a539243
khayden73 <keith73@gmail.com>
2025-12-04 16:44:10 -0500

    filter products by category

--- src/components/Catalog/Products.tsx
+++ src/components/Catalog/Products.tsx
@@ -1,25 +1,55 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { CatalogContext } from "./CatalogProvider.tsx";
 import Styles from "./Products.module.css";
 import { ProductCard } from "./ProductCard.tsx";
+import type { Product } from "../../lib/types.ts";
 
 function Products() {
   const { catalog, isLoading, getCategories } = useContext(CatalogContext);
+  const [filteredProducts, setFilteredProducts] = useState<Product[]>(catalog);
+  const [selectedCategory, setSelectedCategory] = useState<string>("");
 
   if (isLoading) return <p>Loading...</p>;
+
+  const handleCategorySelection = (category: string) => {
+    if (category === selectedCategory) {
+      setSelectedCategory("");
+    } else {
+      setSelectedCategory(category);
+    }
+  };
+
+  useEffect(() => {
+    if (selectedCategory === "") {
+      setFilteredProducts(catalog);
+    } else {
+      setFilteredProducts(
+        catalog.filter((product) => product.category === selectedCategory),
+      );
+    }
+  }, [selectedCategory]);
 
   return (
     <>
       <h2>Products</h2>
-      <div className={Styles.productsList}>
+      <div className={Styles.productCategoryFilter}>
         <ul>
           {getCategories().map((category) => (
-            <li key={category}>{category}</li>
+            <li key={category}>
+              <button
+                className={category === selectedCategory ? Styles.selected : ""}
+                onClick={() => handleCategorySelection(category)}
+              >
+                {category}
+              </button>
+            </li>
           ))}
         </ul>
-        {/*{catalog.map((product) => (*/}
-        {/*  <ProductCard key={product.productId} product={product} />*/}
-        {/*))}*/}
+      </div>
+      <div className={Styles.productsList}>
+        {filteredProducts.map((product) => (
+          <ProductCard key={product.productId} product={product} />
+        ))}
       </div>
     </>
   );
